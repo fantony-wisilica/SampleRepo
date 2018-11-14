464f89b7825493c66f1a5b24b04881711391c84c
francis-Wisilica <fantony@wisilica.com>
2018-11-14 22:01:07 +0530

    Some other change

--- udpclient.c
+++ udpclient.c
@@ -22,7 +22,6 @@ int main(int argc, char **argv) {
 		char *hostname;
 		char buf[BUFSIZE];
 
-		/* check command line arguments */
 		if (argc != 3) {
 				fprintf(stderr,"usage: %s <hostname> <port>\n", argv[0]);
 				exit(0);
@@ -30,31 +29,26 @@ int main(int argc, char **argv) {
 		hostname = argv[1];
 		portno = atoi(argv[2]);
 
-		/* socket: create the socket */
 		sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 		if (sockfd < 0) 
 				error("ERROR opening socket");
 
-		/* gethostbyname: get the server's DNS entry */
 		server = gethostbyname(hostname);
 		if (server == NULL) {
 				fprintf(stderr,"ERROR, no such host as %s\n", hostname);
 				exit(0);
 		}
 
-		/* build the server's Internet address */
 		bzero((char *) &serveraddr, sizeof(serveraddr));
 		serveraddr.sin_family = AF_INET;
 		bcopy((char *)server->h_addr, 
 						(char *)&serveraddr.sin_addr.s_addr, server->h_length);
 		serveraddr.sin_port = htons(portno);
 
-		/* get a message from the user */
 		bzero(buf, BUFSIZE);
 		printf("Please enter msg: ");
 		fgets(buf, BUFSIZE, stdin);
 
-		/* send the message to the server */
 		serverlen = sizeof(serveraddr);
 		n = sendto(sockfd, buf, strlen(buf), 0, &serveraddr, serverlen);
 		if (n < 0) 
